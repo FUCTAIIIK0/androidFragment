edb6d8da53450de8861f8f5ae36d47f4ab9297c2
ig <ig@pss.one>
2019-09-13 12:45:18 +0300

    add tabs Call,Group,Favorite

--- app/src/main/java/com/example/fragmentstutorial/MainActivity.java
+++ app/src/main/java/com/example/fragmentstutorial/MainActivity.java
@@ -2,13 +2,54 @@ package com.example.fragmentstutorial;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ViewPagerAdapter viewPagerAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tablayout_id);
+        viewPager = findViewById(R.id.viewpager_id);
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        //Add fragment
+
+        viewPagerAdapter.AddFragment(new FragmentCall(), "");
+        viewPagerAdapter.AddFragment(new FragmentContact(), "");
+        viewPagerAdapter.AddFragment(new FragmentFav(), "");
+
+
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_call);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_group);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_star);
+
+        //Remove shadow from ActionBar
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setElevation(0);
+
+
+
     }
+
+//    public void onClick(View view){
+//        Log.d("temp", "onClick: ");
+//
+//    }
 }
